00520d1f1f8fbeb85d992368022017f046e85a8e
Qudrat377 <assomad377@gmail.com>
2025-10-29 23:11:39 +0500

    obj proto qo'shildi

---
+++ obj-proto.js
@@ -0,0 +1,30 @@
+// --------------------------------------------------obj uchun prototype
+
+// objuchun includes metodi
+
+// Object.prototype.bormi = function (value) {
+//   for (let key in this) {
+//     if (this[key] === value[key]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// Array.prototype.bormi = function (value) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (this[i] === value) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// let obj = { name: "nodir", age: 25 };
+// let summ = { name: "nodir" };
+
+// let arr = [77, 43, 55, 21, 96];
+
+// console.log(arr.bormi(43)); // ✅ true
+// console.log(obj.bormi(summ)); // ✅ true
+
